621265bc0ba5a98144116f4b58326055ab3c7d9d
shh738 <shh738@mail.usask.ca>
2021-04-01 22:38:29 -0600

    Implemented skeleton code that opens and closes chatbot with certain placeholders that may be replaced later

---
+++ Start.py
@@ -0,0 +1,76 @@
+from uwuBotFunc import EngToUwu
+
+def printOptions():
+    """
+    Function to print the main options
+    We can discuss if this is what you were thinking or whether its more spontaneous
+    """
+    print()
+    print("Choose an option from the list below:")
+    print("1. Translate text from english to uwu")
+    print("2. Start the chatbot")
+    print("3. Quit the application")
+    return input(">")
+
+def translator(string):
+    output = EngToUwu(string)
+    print(output)
+    print()
+
+def chatbot():
+    print()
+    print("Functionality not implemented yet")
+    print()
+
+
+def start():
+    """
+    A function that opens and closes the chatbot
+    we can discuss later whether we need this or not
+    """
+    print("*****************************************************")
+    print("Welcome to Uwu Chat") # we can discuss better names later - this is just a placeholder
+    print("*****************************************************")
+    print()
+    print("type q: to quit specific mode of chatbot") # again 'q:' is just a placeholder, we can discuss later how exactly users will interact with system
+    print()
+
+
+    run = True
+    choice = 0
+    feature_running = False
+    while(run):
+        # only print the main options if the user is not interacting with the chatbot or translating text
+        if not feature_running:
+            choice = printOptions()
+            # assume user chooses a valid feature
+            feature_running = True
+
+        if choice == "1":
+            string = input("Enter text to translate>")
+            # translate text; otherwise go back to home screen if user wishes to quit
+            if not string == 'q:':
+                translator(string)
+            else:
+                feature_running = False
+
+
+        elif choice == "2":
+            chatbot()
+            feature_running = False
+
+        elif choice == "3":
+            run= False
+            print("Thanks for using Uwu Chat")
+            print("Goodbye")      
+
+        else:
+            # invalid entry
+            feature_running = False
+            print()
+            print("That is not a valid option")
+            print()
+
+if __name__ == "__main__":
+    start()
+
